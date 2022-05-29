b792453579b98f9e0c2684572efef08fde2cb496
Wang Zihao <wzhjay@gmail.com>
2022-05-29 19:15:35 +0800

    Add wait_for_db command and its tests, config database connection under app setting

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,9 +4,10 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y and return result """
+    """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
-    """Subtract x from y and return result"""
+    """Subtract x from y and return result."""
     return y - x
