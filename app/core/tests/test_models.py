8ff511333e66064ab4279473279e15e2d41c485a
Wang Zihao <wzhjay@gmail.com>
2022-06-12 18:08:13 +0800

    minor change after diff check

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,12 +31,12 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test email is normalized for new user"""
+        """Test email is normalized for new user."""
         sample_emails = [
-            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
-            ['test4@example.com', 'test4@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
